a2b0afbff08771fd5d6e747d570b72c99c574e11
Jongwoo <han980817@gmail.com>
2019-02-10 11:59:45 +0900

    챗봇 응답 유지보수

--- chatbot/views.py
+++ chatbot/views.py
@@ -9,19 +9,18 @@ from django.utils.decorators import method_decorator
 
 
 #  ------------------------ Fill this with your page access token! -------------------------------
-PAGE_ACCESS_TOKEN = "EAAEYZClBnc0YBAMnly5WbnWIGLfZCTyUGH3Db2L2ZCDun46b8pfRyyYB0mdg3m7ZAkCjZAz2I85XrA1qWALMHekIMcVaUDFBkfAiOiAFzIv4TVu7BH2Qhbx5hCR4ZA1JONDGuJZBR4Ua7lKo59OjwwcZB6ttARy2oAxt95EgMiMqfQZDZD"
+PAGE_ACCESS_TOKEN = "EAAEYZClBnc0YBAIzr5cKepzIs1IGZCW5xDXDvR9OTDyFnmvL7kyaxDv9D9vIfyRxxtpwV1NxYbKEXIQaPJhAC5ZBL7RDLjZB1qyjTXFXZBAkTxJmUdGCLa51LYTjhQ8h2d7Ixz9fG2v5hGLmFCpFZAm6YK22ZB0XItfFvT97cb8VAZDZD"
 VERIFY_TOKEN = "1234567890"
 
-'''
-# 한글화 작업
+
 questions = {
-        '활동 기간': ["""2019.03 - 2020.02\n(1년 활동 후 수료증 발급)"""],
-        '모집 대상': ["""명지대 인문캠퍼스 재학생/휴학생 중 웹 서비스를 만들고 싶은 사람 누구나!!\n1년 동안 꾸준히, 열심히, 열정적으로 활동 할 수 있는 사람을 모집합니다!!"""],
-        '모집 일정': ["""2019.02.20 – 2019.03.07\n1차 서류, 2차 면접 평가로 진행됩니다.\n구체적인 모집 일정 및 지원방법은 추후 업로드 될 예정입니다."""],
-        '1학기 운영 계획': ["""교육\nHTML & CSS (Bootstrap)\nPython\nDjango 를 이용한 웹사이트 개발\n주 1회 또는 2회 스터디 진행\n7기 모집 후에 코드카데미 HTML & CSS 부분 과제 진행합니다.\n미리 해 오셔도 괜찮습니다."""],
-        '멋쟁이 사자처럼 일정': ["""일정\n2월 - 20일 멋쟁이사자처럼 7기 모집 시작\n 3월 - 전체 OT 진행\n7월 - 아이디어톤\n8월 - 무박 2일 해커톤\n11월 - 멋쟁이사자처럼 컨퍼런스 (예정)"""]
+        'a': ["""2019.03 - 2020.02\n(1년 활동 후 수료증 발급)"""],
+        'b': ["""명지대 인문캠퍼스 재학생/휴학생 중 웹 서비스를 만들고 싶은 사람 누구나!!\n1년 동안 꾸준히, 열심히, 열정적으로 활동 할 수 있는 사람을 모집합니다!!"""],
+        'c': ["""2019.02.20 – 2019.03.07\n1차 서류, 2차 면접 평가로 진행됩니다.\n구체적인 모집 일정 및 지원방법은 추후 업로드 될 예정입니다."""],
+        'd': ["""교육\nHTML & CSS (Bootstrap)\nPython\nDjango 를 이용한 웹사이트 개발\n주 1회 또는 2회 스터디 진행\n7기 모집 후에 코드카데미 HTML & CSS 부분 과제 진행합니다.\n미리 해 오셔도 괜찮습니다."""],
+        'e': ["""일정\n2월 - 20일 7기 모집 시작3월 - 전체 OT 진행\n7월 - 아이디어톤\n8월 - 무박 2일 해커톤\n11월 - 멋쟁이사자처럼 컨퍼런스 (예정)"""]
          }
-'''
+
 
 # Helper function
 def post_facebook_message(fbid, recevied_message):
@@ -33,12 +32,15 @@ def post_facebook_message(fbid, recevied_message):
             question_text = random.choice(questions[token])
             break
     if not question_text:
-        question_text = "활동 기간, 모집 대상, 모집 일정, 1학기 운영 계획, 멋쟁이 사자처럼 일정 중 한 가지 키워드를 입력해주세요."
+        question_text = "안녕하세요, 명지대학교(서울)\n멋쟁이 사자처럼 챗봇입니다.\na. 활동 기간\nb. 모집 대상\nc. 모집 일정\nd. 1학기 운영 계획\ne. 멋쟁이 사자처럼 일정\n중 한 가지 알파벳을 입력해주세요."
 
+    '''
+    # 유저 이름 출력(오류 발생, 구현 예정)    
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
     user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
     user_details = requests.get(user_details_url, user_details_params).json()
-    question_text = '질문 주셔서 감사합니다, '+user_details['first_name']+'님! ' + question_text
+    question_text = '질문 주셔서 감사합니다, '+user_details['first_name']+'님!' + question_text
+    '''
 
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":question_text}})
--- likelionmyongji/settings.py
+++ likelionmyongji/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '%71ba)7$c$9f+cj_3pyt9d%*(xyslu31%9dq(mpg%ehp@&6h&@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] # ngrok 실행 시 나오는 주소
+ALLOWED_HOSTS = [
+    '3c937280.ngrok.io',
+] # ngrok 실행 시 나오는 주소
 
 
 # Application definition
