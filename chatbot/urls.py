b2e2f7aa14432bf02c2787979eed5e9f2f6576d1
Saad Mirza <44167576+unifiedsaad@users.noreply.github.com>
2019-03-20 14:42:58 +0500

    update url

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from .views import chatbotView
 
 urlpatterns = [
-    url(r'^07212a3b56af23fd34af882775a00e222097411895e28f5301/?$', chatbotView.as_view())
+    url(r'^api/?$', chatbotView.as_view())
 ]
